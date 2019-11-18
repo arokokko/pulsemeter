55879869e42469494949742998f511a98a3b06f3
Alex Tihonov <arokokko@gmail.com>
2019-11-18 17:41:14 +0700

    Tiny slider created and adapted

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,44 @@
-$(document).ready(function() {
-    $('.carousel__inner').slick( {
-      infinite: false,
-      speed: 1200,
-      adaptiveHeight: true,
-      prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev-arrow.png"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="icons/next-arrow.png"></button>',
-      responsive: [
-        {
-          breakpoint: 780,
-          settings: {
-            arrows: false
-          }
-        }
-      ]
-    });
-  });
+// $(document).ready(function() {
+//     $('.carousel__inner').slick( {
+//       infinite: false,
+//       speed: 1200,
+//       adaptiveHeight: true,
+//       prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev-arrow.png"></button>',
+//       nextArrow: '<button type="button" class="slick-next"><img src="icons/next-arrow.png"></button>',
+//       responsive: [
+//         {
+//           breakpoint: 780,
+//           settings: {
+//             arrows: false
+//           }
+//         }
+//       ]
+//     });
+//   });
+
+const slider = tns({
+	container: '.carousel-ts__inner',
+	items: 1,
+	slideBy: 'page',
+	autoplay: false,
+	autoHeight: true,
+	controls: false,
+	nav: false,
+	navPosition: 'bottom',
+	responsive: {
+		320: {
+			nav: true
+		},
+		780: {
+			nav: false
+		}
+	}
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+	slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+	slider.goTo('next');
+});
