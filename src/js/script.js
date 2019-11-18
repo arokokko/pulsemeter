9f7f1f29b60d596b7e35cf9d7123da7a9fe6f9bb
Alex Tihonov <arokokko@gmail.com>
2019-11-18 15:10:14 +0700

    Slick slider created and adapted without dots

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick();
+$(document).ready(function() {
+    $('.carousel__inner').slick( {
+      infinite: false,
+      speed: 1200,
+      adaptiveHeight: true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev-arrow.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/next-arrow.png"></button>',
+      responsive: [
+        {
+          breakpoint: 780,
+          settings: {
+            arrows: false
+          }
+        }
+      ]
+    });
   });
