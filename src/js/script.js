eefe0c2ac3b3bf1f626bd5abe2a031b98f4ce426
Alex Tihonov <arokokko@gmail.com>
2019-11-19 17:52:13 +0700

    Slick slider deteled

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,3 @@
-// $(document).ready(function() {
-//     $('.carousel__inner').slick( {
-//       infinite: false,
-//       speed: 1200,
-//       adaptiveHeight: true,
-//       prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev-arrow.png"></button>',
-//       nextArrow: '<button type="button" class="slick-next"><img src="icons/next-arrow.png"></button>',
-//       responsive: [
-//         {
-//           breakpoint: 780,
-//           settings: {
-//             arrows: false
-//           }
-//         }
-//       ]
-//     });
-//   });
-
 const slider = tns({
 	container: '.carousel-ts__inner',
 	items: 1,
