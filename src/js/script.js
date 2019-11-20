1db85d014090808f5a9fd2faa0d322977c748bbc
Alex Tihonov <arokokko@gmail.com>
2019-11-20 15:06:36 +0700

    Tabs created and connected

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// tiny slider
 const slider = tns({
 	container: '.carousel-ts__inner',
 	items: 1,
@@ -24,3 +25,33 @@ document.querySelector('.prev').addEventListener('click', function () {
 document.querySelector('.next').addEventListener('click', function () {
 	slider.goTo('next');
 });
+
+
+$(document).ready(function(){
+	// tabs
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+		$(this)
+		  .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+		  .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
+
+	// переключатели подробнее и назад в карточках
+	function toggleSlide(item) {
+		$(item).each(function(i) {
+			$(this).on('click', function(e) {
+				e.preventDefault();
+				$('.card__content').eq(i).toggleClass('card__content_active');
+				$('.card__info').eq(i).toggleClass('card__info_active');
+			});
+		});
+	}
+	
+	toggleSlide('.card__more');
+	toggleSlide('.card__goback');
+
+});
+
+
+
+
+
